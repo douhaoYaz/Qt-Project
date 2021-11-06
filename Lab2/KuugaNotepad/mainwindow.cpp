ee956feb6086f976567261c68f74ad9a47334023
douhaoYaz <fusionyaz@163.com>
2021-11-06 13:42:17 +0800

    Finish new, open, save and saveas

--- Lab2/KuugaNotepad/mainwindow.cpp
+++ Lab2/KuugaNotepad/mainwindow.cpp
@@ -3,6 +3,11 @@
 #include "aboutdialog.h"
 #include "searchdialog.h"
 #include "replacedialog.h"
+#include <QFileDialog>
+#include <QFile>
+#include <QMessageBox>
+#include <QTextStream>
+
 
 
 MainWindow::MainWindow(QWidget *parent)
@@ -10,6 +15,11 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // TODO 一运行程序，WindowTitle就有个*号，即textEdit以发生变化，为什么会出现这种情况，需要debug一下
+
+    textChanged = false;    // 将textEdit的是否被修改状态初始化为False
+    on_actionNew_triggered();
 
     // 设置编辑器右下角状态栏要显示的信息
     // 显示长度和行数
@@ -54,4 +64,152 @@ void MainWindow::on_actionReplace_triggered()
     ReplaceDialog dlg;
     dlg.exec();
 }
+
+
+void MainWindow::on_actionNew_triggered()
+{
+    // 在新建文件之前，先让用户确认一下
+    if(!userEditConfirmed())
+        return;
+
+    filePath = "";
+    ui->textEdit->clear();
+    this->setWindowTitle(tr("新建文本文件 - 编辑器"));
+
+    // 新建文件后，还需要把textChanged重置为false，，因为上面的操作会使textEdit改变，引起textChanged信号
+    // textEdit的textChanged槽函数会把textChanged全局变量置为true
+    textChanged = false;
+}
+
+
+void MainWindow::on_actionOpen_triggered()
+{
+    // 在打开文件之前，先让用户确认一下
+    if(!userEditConfirmed())
+        return;
+
+    QString filename = QFileDialog::getOpenFileName(this, "打开文件", ".", tr("Text files (*.txt) ;; All (*.*)"));
+    QFile file(filename);
+
+    if(!file.open(QFile::ReadOnly | QFile::Text)){
+        QMessageBox::warning(this, "..", "打开文件失败");
+        return;
+    }
+
+    filePath = filename;
+
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->insertPlainText(text);
+    file.close();
+
+    this->setWindowTitle(QFileInfo(filename).absoluteFilePath());
+
+    // 打开文件后，还需要把textChanged重置为false，因为上面的操作会使textEdit改变，引起textChanged信号
+    // textEdit的textChanged槽函数会把textChanged全局变量置为true
+    textChanged = false;
+}
+
+
+void MainWindow::on_actionSave_triggered()
+{
+    QFile file(filePath);
+
+    if(filePath == ""){
+        QString filename = QFileDialog::getSaveFileName(this, "保存文件", ".", tr("Text files (*.txt) "));
+
+        QFile file(filename);
+        if(!file.open(QFile::WriteOnly | QFile::Text)){
+            QMessageBox::warning(this, "..", "打开保存文件失败");
+            return;
+        }
+        file.close();
+        filePath = filename;
+    }
+    else if(!file.open(QFile::WriteOnly | QFile::Text)){
+        QMessageBox::warning(this, "..", "打开文件失败");
+
+        QString filename = QFileDialog::getSaveFileName(this, "保存文件", ".", tr("Text files (*.txt) "));
+
+        QFile file(filename);
+        if(!file.open(QFile::WriteOnly | QFile::Text)){
+            QMessageBox::warning(this, "..", "打开保存文件失败");
+            return;
+        }
+        filePath = filename;
+    }
+
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out<<text;
+    file.flush();
+    file.close();
+
+    this->setWindowTitle(QFileInfo(filePath).absoluteFilePath());
+
+    textChanged = false;
+
+    // TODO 保存的函数还需要改进补充逻辑：没有新建文件的情况下按保存的逻辑
+}
+
+
+void MainWindow::on_actionSavaAs_triggered()
+{
+    QString filename = QFileDialog::getSaveFileName(this, "保存文件", ".", tr("Text files (*.txt) "));
+
+    QFile file(filename);
+    if(!file.open(QFile::WriteOnly | QFile::Text)){
+        QMessageBox::warning(this, "..", "打开保存文件失败");
+        return;
+    }
+
+    filePath = filename;
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out<<text;
+    file.flush();
+    file.close();
+
+    this->setWindowTitle(QFileInfo(filePath).absoluteFilePath());
+}
+
+
+void MainWindow::on_textEdit_textChanged()
+{
+    if(!textChanged){
+        this->setWindowTitle("*" + this->windowTitle());
+        textChanged = true;
+    }
+}
+
+bool MainWindow::userEditConfirmed()
+{
+    if(textChanged){
+
+        QString path = (filePath != "") ? filePath : "无标题.txt";
+
+        QMessageBox msg(this);
+        msg.setIcon(QMessageBox::Question);
+        msg.setWindowTitle("...");
+        msg.setWindowFlag(Qt::Drawer);
+        msg.setText(QString("是否将更改保存到\n") + "\"" + path + "\" ?");
+        msg.setStandardButtons(QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel);
+
+        int r = msg.exec();
+        switch(r){
+        case QMessageBox::Yes:
+            on_actionSave_triggered();
+            break;
+        case QMessageBox::No:
+            textChanged = false;
+            break;
+        case QMessageBox::Cancel:
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
 
--- Lab2/KuugaNotepad/mainwindow.h
+++ Lab2/KuugaNotepad/mainwindow.h
@@ -23,10 +23,25 @@ private slots:
 
     void on_actionReplace_triggered();
 
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionSavaAs_triggered();
+
+    void on_textEdit_textChanged();
+
 private:
     Ui::MainWindow *ui;
 
     QLabel statusCursorLabel;   //用于在状态栏中显示信息
     QLabel statusLabel;         //用于在状态栏中显示长度和行数信息
+
+    QString filePath;           // 记录成功打开的文件路径，用于Open操作和Save操作
+    bool textChanged;           // 用于记录textEdit是否被改变
+
+    bool userEditConfirmed();   // 在用户点击新建、打开之后弹出MessageBox，让用户选择是否保存当前更改
 };
 #endif // MAINWINDOW_H
