38917de04cdcc064a433fb96fce26b87310888ee
douhaoYaz <fusionyaz@163.com>
2021-12-01 00:05:15 +0800

    Finish UI interface layout, toolbar and statusbar

--- Lab3-WeatherInfoDispaly/mainwindow.cpp
+++ Lab3-WeatherInfoDispaly/mainwindow.cpp
@@ -6,6 +6,25 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    labelStationInfo = new QLabel("当前气象站点： ");  // 状态栏显示鼠标坐标
+    labelStationInfo->setAlignment(Qt::AlignCenter);
+    labelStationInfo->setMinimumWidth(400);
+    labelStationInfo->setMaximumWidth(400);
+
+    ui->statusbar->addPermanentWidget(labelStationInfo);
+
+    labXYValue = new QLabel("日期=， 温度= ");   // 状态栏显示鼠标坐标
+    labXYValue->setAlignment(Qt::AlignCenter);
+    labXYValue->setMinimumWidth(400);
+    labXYValue->setMaximumWidth(400);
+
+    ui->statusbar->addPermanentWidget(labXYValue);
+
+    QLabel *author = new QLabel(ui->statusbar);
+    author->setText(tr("201841510108 符浩扬"));
+    ui->statusbar->addPermanentWidget(author);
+
 }
 
 MainWindow::~MainWindow()
--- Lab3-WeatherInfoDispaly/mainwindow.h
+++ Lab3-WeatherInfoDispaly/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,8 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    QLabel *labXYValue; // 状态栏显示鼠标坐标处的日期和温度
+    QLabel *labelStationInfo;   // 状态栏显示鼠标坐标处的气象站点
 };
 #endif // MAINWINDOW_H
