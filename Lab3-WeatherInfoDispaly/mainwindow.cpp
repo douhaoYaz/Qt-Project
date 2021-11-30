225333748f7feb99477f779f2d6beed54431e4b2
douhaoYaz <fusionyaz@163.com>
2021-12-01 01:05:52 +0800

    Read csv and parse data

--- Lab3-WeatherInfoDispaly/mainwindow.cpp
+++ Lab3-WeatherInfoDispaly/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QTextStream>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -30,5 +33,60 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_action_triggered()
+{
+    //打开文件
+    QString curPath=QCoreApplication::applicationDirPath(); //获取应用程序的路径
+    //调用打开文件对话框打开一个文件
+    QString aFileName=QFileDialog::getOpenFileName(this,"打开一个文件",curPath,
+                 "气象数据文件(*.csv);;所有文件(*.*)");
+    if (aFileName.isEmpty())
+        return; //如果未选择文件，退出
+
+    QFile aFile(aFileName);  //以文件方式读出
+    if (aFile.open(QIODevice::ReadOnly | QIODevice::Text)) //以只读文本方式打开文件
+    {
+        QTextStream aStream(&aFile); //用文本流读取文件
+        while (!aStream.atEnd())
+        {
+            QString str=aStream.readLine();//读取文件的一行
+//            qDebug() << str;
+            processRecord(str);
+        }
+
+        for(auto key: weatherRecordsMap.keys())
+        {
+            qDebug() << key << ":" << weatherRecordsMap[key].size();
+        }
+        aFile.close();//关闭文件
+    }
+}
+
+void MainWindow::processRecord(QString recordStr)
+{
+    // 一个或多个空格、TAB等分隔符隔开的字符串， 分解为一个StringList
+    QStringList tmpList=recordStr.split(QRegExp(","));
+    WeatherRecord record;
+
+    // 简单的数据有效性检测
+    if(tmpList.size() != 9){
+        qDebug() << "wrong format" << recordStr;
+        return;
+    }
+
+    record.date = tmpList[0];
+    record.key = tmpList[1];
+    record.average_temperature = tmpList[2].toDouble();
+    record.minimum_temperature = tmpList[3].toDouble();
+    record.maximum_temperature = tmpList[4].toDouble();
+    record.rainfall = tmpList[5].toDouble();
+    record.snowfall = tmpList[6].toDouble();
+    record.dew_point = tmpList[7].toDouble();
+    record.relative_humidity = tmpList[8].toDouble();
+
+    weatherRecordsMap[record.key].append(record);
 }
 
--- Lab3-WeatherInfoDispaly/mainwindow.h
+++ Lab3-WeatherInfoDispaly/mainwindow.h
@@ -3,10 +3,23 @@
 
 #include <QMainWindow>
 #include <QLabel>
+#include <QMap>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+typedef struct WeatherRecord{
+    QString date;
+    QString key{"UNKNOWN"};
+    double average_temperature{0};
+    double minimum_temperature{0};
+    double maximum_temperature{0};
+    double rainfall{0};
+    double snowfall{0};
+    double dew_point{0};
+    double relative_humidity{0};
+}WeatherRecord;
 
 class MainWindow : public QMainWindow
 {
@@ -16,10 +29,17 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_action_triggered();
+
 private:
+
+    void processRecord(QString str);
     Ui::MainWindow *ui;
 
     QLabel *labXYValue; // 状态栏显示鼠标坐标处的日期和温度
     QLabel *labelStationInfo;   // 状态栏显示鼠标坐标处的气象站点
+
+    QMap<QString, QList<WeatherRecord>> weatherRecordsMap;
 };
 #endif // MAINWINDOW_H
